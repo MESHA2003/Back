4217dce60e2167d5622eff97f3dd269866e0c8a6
MESHA2003 <bitam11-083@suza.ac.tz>
2025-02-06 14:48:48 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +27,6 @@ SECRET_KEY = 'django-insecure-qxpkjl9&+&5i2ikdmw@%q9vnduz7#7fakb31m$o$_zy7cn7$w$
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = ['https://dainess.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'MyDiary',
     'rest_framework',
     'rest_framework.authtoken',
+    'whitenoise',
     
 
 ]
@@ -60,14 +62,9 @@ MIDDLEWARE = [
 
 
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    
-]
+CORS_ALLOWED_ORIGINS = True
 
 ROOT_URLCONF = 'project.urls'
 # settings.py
@@ -97,11 +94,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
